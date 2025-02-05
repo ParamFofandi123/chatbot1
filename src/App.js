2c7d8587bb24fa2919fcfdb5717b494fa44d3d3b
ParamFofandi123 <paramfofandi@gmail.com>
2025-02-05 11:04:56 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Signup } from './components/Signup/Signup';
 import {Login} from './components/Login/Login';
 import {Chatbot} from './components/Chatbot/Chatbot';
